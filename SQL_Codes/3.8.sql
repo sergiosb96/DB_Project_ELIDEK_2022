792ca33c81215e7c21a61b11be58919b664d2c43
GinaKalog <106018032+GinaKalog@users.noreply.github.com>
2022-05-25 19:07:10 +0300

    Update 3.8.sql

--- SQL_Codes/3.8.sql
+++ SQL_Codes/3.8.sql
@@ -1,10 +1,8 @@
-SELECT first_name, last_name, COUNT(project_id)
- FROM Researchers
-  JOIN Projects
+SELECT first_name, last_name, COUNT(project_id) AS Total_Projects
+FROM Researchers
+JOIN Projects
   ON Researchers.researcher_id = Projects.researcher_id
- WHERE EXISTS 
-  (SELECT Projects.title FROM Projects JOIN Deliverables ON Projects.project_id = Deliverables.project_id 
+WHERE EXISTS 
+   (SELECT Projects.title FROM Projects JOIN Deliverables ON Projects.project_id = Deliverables.project_id 
    AND deliverables.date IS NULL)
- AND COUNT(project_id) >= 5;
-
-
+HAVING COUNT(project_id) >= 5 ;
