b50929a400bbefa6bf7e947c61117f4abfbd4e30
GinaKalog <106018032+GinaKalog@users.noreply.github.com>
2022-05-25 17:59:55 +0300

    Update 3.6.sql

--- SQL_Codes/3.6.sql
+++ SQL_Codes/3.6.sql
@@ -4,4 +4,5 @@ FROM Researchers
 JOIN Projects
  ON Researchers.researcher_id = Projects.researcher_id
 WHERE birth_date > DATE_SUB(NOW(), INTERVAL 40 year)
+ AND ending_date > NOW()
 ORDER BY COUNT(project_id) DESC;
