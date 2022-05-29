0d48f6071aba614e8fada20e6e202e91608a183a
Azzy <sergiosbatsas@gmail.com>
2022-05-30 01:11:02 +0300

    app_start

---
+++ Project/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+
+require('custom-env').env('localhost');
+
+/* ROUTES and how to import routes */
+
+const layout = require('./routes/layout');
+const grades = require('./routes/grades');
+const students = require('./routes/students');
+
+/* end of ROUTES and how to import routes */
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(flash());
+
+app.use(session({
+    secret: "ThisShouldBeSecret",
+    resave: false,
+    saveUninitialized: false
+}));
+
+/* Routes used by the project */
+
+app.use('/', layout);
+app.use('/grades', grades);
+app.use('/students', students);
+
+/* End of routes used by the project */
+
+// In case of an endpoint does not exist must return 404.html
+app.use((req, res, next) => { res.status(404).render('404.ejs', { pageTitle: '404' }) })
+
+module.exports = app;
