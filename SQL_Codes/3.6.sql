5e2a29c1f12943746d78c55ed16e157edd0ec330
GinaKalog <106018032+GinaKalog@users.noreply.github.com>
2022-05-30 15:12:32 +0300

    Update 3.6.sql
    teliko, xreiazomaste data gia na to epikairopoiisoume

--- SQL_Codes/3.6.sql
+++ SQL_Codes/3.6.sql
@@ -1,9 +1,10 @@
-SELECT first_name, last_name, COUNT(Projects.project_id) AS Total_Projects
+SELECT CONCAT(Researchers.first_name, ' ' , Researchers.last_name) AS Full_Name, COUNT(Projects.project_id) AS Total_Projects
 FROM Researchers
  JOIN Working
   ON Researchers.researcher_id = Working.researcher_id
  INNER JOIN  Projects 
   ON Working.project_id = Projects.project_id
 WHERE birth_date > DATE_SUB(NOW(), INTERVAL 40 year)
- AND NOW() BETWEEN starting_date AND ending_date
+ AND CURDATE() BETWEEN starting_date AND ending_date
+GROUP BY Full_Name
 ORDER BY COUNT(Projects.project_id) DESC;
