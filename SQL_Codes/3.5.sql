ecd869e26e552eb1703d6f75ef64aef7eec68d24
NikosP6 <106018110+NikosP6@users.noreply.github.com>
2022-05-30 15:03:11 +0300

    Update 3.5.sql

--- SQL_Codes/3.5.sql
+++ SQL_Codes/3.5.sql
@@ -1,13 +1,12 @@
-
-
-select count(distinct t.name,s.name) as times,t.name,s.name
+select t.name,s.name, COUNT(*)
 from (select projects.project_id,name 
-	from projects,science_field
-	where projects.project_id= science_field.project_id) as T,
-	(select projects.project_id,name 
-	from projects,science_field
-	where projects.project_id= science_field.project_id) as S
+    from projects,science_field
+    where projects.project_id= science_field.project_id) as T,
+    (select projects.project_id,name 
+    from projects,science_field
+    where projects.project_id= science_field.project_id) as S
 where T.project_id=S.project_id and T.name<>S.name
 group by s.name
-order by times desc
+order by count(*) desc
 limit 3;
+
