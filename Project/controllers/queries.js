69ec624ef483142934e2019228062c149c15f9b8
Azzy <sergiosbatsas@gmail.com>
2022-06-02 23:23:59 +0300

    updated_README

--- Project/controllers/queries.js
+++ Project/controllers/queries.js
@@ -153,7 +153,7 @@ exports.getQuery5 = (req, res, next) => {
     pool.getConnection((err, conn) => {
 
         /* execute query  */
-            conn.promise().query(`select t.name,s.name, COUNT(*) from (select projects.project_id,name from projects,science_field where projects.project_id= science_field.project_id) as T, (select projects.project_id,name  from projects,science_field where projects.project_id= science_field.project_id) as S where T.project_id=S.project_id and T.name<>S.name group by s.name order by count(*) desc limit 3;`)
+            conn.promise().query(`select t.name AS field_1 ,s.name AS field_2 , COUNT(*) AS times from (select projects.project_id,name from projects,science_field where projects.project_id= science_field.project_id) as T, (select projects.project_id,name  from projects,science_field where projects.project_id= science_field.project_id) as S where T.project_id=S.project_id and T.name<>S.name group by s.name order by count(*) desc limit 3;`)
             .then(([rows, fields]) => {
                 res.render('5.ejs', {
                     pageTitle: "Query 5",
