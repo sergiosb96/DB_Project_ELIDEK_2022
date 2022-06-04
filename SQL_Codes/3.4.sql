3a7e2aa3f13a870d065a94a27a6ecba7be055be4
GinaKalog <106018032+GinaKalog@users.noreply.github.com>
2022-06-04 16:44:56 +0300

    Update 3.4.sql

---
+++ SQL_Codes/3.4.sql
@@ -0,0 +1,12 @@
+SELECT A.name, A.Year AS Year_1, B.year AS Year_2, A.total_projects AS Annual_Projects
+FROM 
+   (SELECT organizations.organization_id, organizations.name AS Name, count(*) AS total_projects, year(starting_date) AS Year
+    FROM organizations
+    INNER JOIN projects ON organizations.organization_id = projects.organization_id
+    GROUP BY organization_id, year) AS A,
+   (SELECT organizations.organization_id, organizations.name AS Name, count(*) AS total_projects, year(starting_date) AS Year
+    FROM organizations
+ 	  INNER JOIN projects ON organizations.organization_id = projects.organization_id
+    GROUP BY organization_id, year) AS B
+WHERE A.organization_id = B.organization_id AND A.Year=B.Year+1 AND A.total_projects=B.total_projects 
+GROUP BY A.name;
